5103dd0579590bc87857cbeb656ab62bd19f9879
Jonasuuuuuu <jonas@prosperna.com>
2021-11-09 16:57:11 +0800

    footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../Header.css';
 function Footer() {
     return (
         <div className="footer">
-            ©2021. All rights reserved Onic E-Sports | Indonesia | Philippines | Powered by Xtendly Philippines
+            ©2021. All rights reserved Onic E-Sports | Indonesia | Philippines | Powered by <a href="https://xtendly.com" className="footer-link">Xtendly</a> Philippines
         </div>
     )
 }
